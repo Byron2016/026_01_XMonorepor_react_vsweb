665c2ee27386119a5df82df724a551129b6757f4
Byron 2016 <bgva2005@yahoo.com>
2021-11-08 09:15:20 -0500

    001_sb_005_Add route to new storybook stories files

--- OriginalSeparadoMio/frontend/paquetes/components/.storybook/main.js
+++ OriginalSeparadoMio/frontend/paquetes/components/.storybook/main.js
@@ -1,10 +1,21 @@
+// module.exports = {
+//   "stories": [
+//     "../stories/**/*.stories.mdx",
+//     "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+//   ],
+//   "addons": [
+//     "@storybook/addon-links",
+//     "@storybook/addon-essentials"
+//   ]
+// }
+
+const path = require("path");
+
 module.exports = {
-  "stories": [
+  stories: [
+    "../src/components/**/*.stories.@(js|jsx|ts|tsx)",
     "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+};
