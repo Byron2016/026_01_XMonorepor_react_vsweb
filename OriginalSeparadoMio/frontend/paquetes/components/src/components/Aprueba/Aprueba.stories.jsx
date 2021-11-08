6826893954cbfddbbdf2987dc0b64b899c7b1c3c
Byron 2016 <bgva2005@yahoo.com>
2021-11-08 09:16:08 -0500

    001_sb_006_Add storybook test component

---
+++ OriginalSeparadoMio/frontend/paquetes/components/src/components/Aprueba/Aprueba.stories.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Button from './Aprueba'
+
+export default {
+  title: 'A-Prueba/Button',
+  component: Button,
+}
+
+export const Primary = () => (
+  <Button>APruebad</Button>
+)
